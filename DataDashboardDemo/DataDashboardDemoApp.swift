6726540fe014a753a0f84c5a571615e997a1a383
pgvaghela <pgvaghela20@gmail.com>
2025-06-05 11:20:37 -0600

    Initial Commit

---
+++ DataDashboardDemo/DataDashboardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataDashboardDemoApp.swift
+//  DataDashboardDemo
+//
+//  Created by Priyansh Vaghela on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct DataDashboardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
