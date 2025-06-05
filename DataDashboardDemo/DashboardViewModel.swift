7b738b91fcb239b3e5ee768ba5a1c0535a72fed9
pgvaghela <pgvaghela20@gmail.com>
2025-06-05 11:24:36 -0600

    Initialize project: Xcode SwiftUI App template, add stub files

---
+++ DataDashboardDemo/DashboardViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  DashboardViewModel.swift
+//  DataDashboardDemo
+//
+//  Created by Priyansh Vaghela on 6/5/25.
+//
+
---
+++ DataDashboardDemo/NetworkService.swift
@@ -0,0 +1,7 @@
+//
+//  NetworkService.swift
+//  DataDashboardDemo
+//
+//  Created by Priyansh Vaghela on 6/5/25.
+//
+
